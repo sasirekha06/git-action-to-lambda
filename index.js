24d9228df0edc474be8c46c034ba45b9484dbaa8
sasirekha06 <45126476+sasirekha06@users.noreply.github.com>
2023-01-23 22:44:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
    const response = {
        statusCode: 200,
-       body: JSON.stringify('Hello world nodejs file1 '),
+       body: JSON.stringify('Hello world nodejs updated file2 '),
    };
    return response;
 };
